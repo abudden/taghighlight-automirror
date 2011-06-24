7800a1e51775d40913a069fbdc9723b405f4b56b
A. S. Budden <abudden@gmail.com>
2011-06-24 12:19:25 +0100

    D2U

--- plugin/TagHighlight/version.py
+++ plugin/TagHighlight/version.py
@@ -1,2 +1,2 @@
-revision = "## RevTag:: r473 ##".strip('# ').replace('RevTag::', 'revision')
+revision = "## RevTag:: r474 ##".strip('# ').replace('RevTag::', 'revision')
 date = "## Date:: 24th June 2011 ##".strip('# ').replace('Date:: ', '')
