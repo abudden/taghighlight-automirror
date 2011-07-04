61fab251c5439ec8914f298617d8227e4972c541
A. S. Budden <abudden@gmail.com>
2011-07-04 12:25:08 +0100

    Removed old ctags_highlighting script.

--- dist/make_mktypes_binary.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run from vimfiles directory
-cd .. && (
-if [ -e dist/mktypes_binary.zip ]
-then
-	rm dist/mktypes_binary.zip
-fi
-zip -r dist/mktypes_binary.zip extra_source/mktypes/dist/*)
