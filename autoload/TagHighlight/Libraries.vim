76deb44980a38d29edfda54253885ac334eee76b
A. S. Budden <abudden@gmail.com>
2011-07-25 15:15:51 +0100

    Updated copyright notice.

--- autoload/TagHighlight/Libraries.vim
+++ autoload/TagHighlight/Libraries.vim
@@ -1,12 +1,12 @@
 " Tag Highlighter:
 "   Author:  A. S. Budden <abudden _at_ gmail _dot_ com>
-"   Date:    11/07/2011
+"   Date:    25/07/2011
 "   Version: 1
-" Copyright: Copyright (C) 2011 A. S. Budden
+" Copyright: Copyright (C) 2009-2011 A. S. Budden
 "            Permission is hereby granted to use and distribute this code,
 "            with or without modifications, provided that this copyright
 "            notice is copied with it. Like anything else that's free,
-"            Libraries.vim is provided *as is* and comes with no
+"            the TagHighlight plugin is provided *as is* and comes with no
 "            warranty of any kind, either expressed or implied. By using
 "            this plugin, you agree that in no event will the copyright
 "            holder be liable for any damages resulting from the use
--- plugin/TagHighlight/module/version.py
+++ plugin/TagHighlight/module/version.py
@@ -1,2 +1,18 @@
-revtag = "## RevTag:: r473 ##".strip('# ').replace('RevTag::', 'revision')
-datetag = "## Date:: 4th July 2011  ##".strip('# ').replace('Date:: ', '')
+#!/usr/bin/env python
+# Tag Highlighter:
+#   Author:  A. S. Budden <abudden _at_ gmail _dot_ com>
+#   Date:    25/07/2011
+#   Version: 1
+# Copyright: Copyright (C) 2009-2011 A. S. Budden
+#            Permission is hereby granted to use and distribute this code,
+#            with or without modifications, provided that this copyright
+#            notice is copied with it. Like anything else that's free,
+#            the TagHighlight plugin is provided *as is* and comes with no
+#            warranty of any kind, either expressed or implied. By using
+#            this plugin, you agree that in no event will the copyright
+#            holder be liable for any damages resulting from the use
+#            of this software.
+
+# ---------------------------------------------------------------------
+revtag = "## RevTag:: r554 ##".strip('# ').replace('RevTag::', 'revision')
+datetag = "## Date:: 25th July 2011 ##".strip('# ').replace('Date:: ', '')
